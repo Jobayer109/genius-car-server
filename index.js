129b96ef82bb4f2135ec3c4eaf4e2319d8545005
Nightowlam <nightowl3.00am@gmail.com>
2022-11-02 12:31:57 +0600

    post api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,7 +8,6 @@ app.use(cors());
 app.use(express.json());
 
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nmcknth.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -16,13 +16,31 @@ const client = new MongoClient(uri, {
 });
 
 const dbConnect = async () => {
-    try {
-        const servicesCollection = client.db("GeniusDB").collection("services")
-        
-    }
-    finally {
-        
-    }
+  try {
+    const serviceCollection = client.db("GeniusCar").collection("services");
+    const orderCollection = client.db("GeniusCar").collection("orders");
+
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    //order api
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+  } finally {
+  }
 };
 dbConnect().catch((err) => console.error(err));
 
