9b70e112aabbdc1b06d0788dfd221031d1616d98
Nightowlam <nightowl3.00am@gmail.com>
2022-11-02 22:46:25 +0600

    Alhamdulillah

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -37,6 +37,38 @@ const dbConnect = async () => {
     app.post("/orders", async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+
+    app.get("/orders", async (req, res) => {
+      let query = {};
+
+      if (req.query.email) {
+        query = { email: req.query.email };
+      }
+
+      const cursor = orderCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const status = req.body.status;
+      const updateDoc = {
+        $set:{
+          status: status
+        }
+      };
+      const result = await orderCollection.updateOne(query, updateDoc);
       res.send(result);
     });
   } finally {
